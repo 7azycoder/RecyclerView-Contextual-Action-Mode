0fcf8fe1727340e4f12cb7fbf1692b2639335af8
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-06-23 21:46:05 +0530

    added recyclerview using cardview layout for each row

--- ActionMode/app/src/main/java/com/devlovepreet/actionmode/MainActivity.java
+++ ActionMode/app/src/main/java/com/devlovepreet/actionmode/MainActivity.java
@@ -2,18 +2,45 @@ package com.devlovepreet.actionmode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+    Toolbar toolbar;
+    int[] img_id = {R.drawable.avengers,R.drawable.bajrangi,R.drawable.blackhat,R.drawable.dragons,R.drawable.frankstein,
+            R.drawable.ninja,R.drawable.oculus,R.drawable.water,R.drawable.ongbak,R.drawable.oups,R.drawable.oculus,R.drawable.water,R.drawable.ongbak,R.drawable.oups};
+    ArrayList<Contact> contacts = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        String[] Name,Email;
+        Name = getResources().getStringArray(R.array.name);
+        Email = getResources().getStringArray(R.array.email);
+        int i=0;
+        for(String NAME : Name)
+        {
+            Contact contact = new Contact(img_id[i],NAME,Email[i]);
+            contacts.add(contact);
+            i++;
+        }
+        adapter = new ContactAdapter(contacts,MainActivity.this);
+        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
